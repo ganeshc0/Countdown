7c49aa8986183f1edea2082a69297410d19f3496
royalgk <jitu45290@gmail.com>
2023-03-11 11:20:36 +0530

    add css js and index.html

---
+++ js/index.js
@@ -0,0 +1,31 @@
+const endDate ="11 March 2023  10:30 pm"; 
+
+document.getElementById('endDate').innerHTML=endDate;
+const input =document.querySelectorAll('input');
+
+function clock(){
+    const end = new Date(endDate);
+    const now = new Date();
+    const diff =(end-now)/1000;
+    
+    // console.log('end');
+    // console.log('now');
+
+    if(diff < 0) return;
+    
+    input[0].value=Math.floor(diff / 3600 / 24);
+    input[1].value=Math.floor(diff/3600)%24;
+    input[2].value=Math.floor(diff/60)%60;
+    input[3].value=Math.floor(diff)%60;
+}
+// convert days
+// 1 day =24hr
+// 1 hr= 60min
+// 60min =3600 sec
+clock();
+
+setInterval(
+    ()=>{
+        clock();
+    },1000
+)
