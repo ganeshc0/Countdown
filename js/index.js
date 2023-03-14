b742afee06d484b57ef6598a5c48042729ad887b
royalgk <jitu45290@gmail.com>
2023-03-14 10:54:55 +0530

    update css and js

--- js/index.js
+++ js/index.js
@@ -1,4 +1,4 @@
-const endDate ="11 March 2023  10:30 pm"; 
+const endDate ="30 March 2023  10:30 pm"; 
 
 document.getElementById('endDate').innerHTML=endDate;
 const input =document.querySelectorAll('input');
@@ -13,10 +13,10 @@ function clock(){
 
     if(diff < 0) return;
     
-    input[0].value=Math.floor(diff / 3600 / 24);
-    input[1].value=Math.floor(diff/3600)%24;
-    input[2].value=Math.floor(diff/60)%60;
-    input[3].value=Math.floor(diff)%60;
+    input[0].value=Math.floor(diff / 3600 / 24);// convert day
+    input[1].value=Math.floor(diff/3600)%24;// convert hours
+    input[2].value=Math.floor(diff/60)%60;//convert min
+    input[3].value=Math.floor(diff)%60;// convert sec
 }
 // convert days
 // 1 day =24hr
